9867279fd3b62600dd3bb225a002fc4e5beefa4f
won <wjin@nyit.edu>
2014-11-24 16:19:52 -0500

    genrated add_user_id to articles model and created one to has_many association btwn User and articles models

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,8 @@ class Article < ActiveRecord::Base
   validates_presence_of :title
   validates_presence_of :body
 
+  belongs_to :user
+
   def long_title
     "#{title} - #{published_at}"
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124204557) do
+ActiveRecord::Schema.define(version: 20141124211434) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20141124204557) do
     t.datetime "updated_at"
     t.string   "excerpt"
     t.string   "location"
+    t.integer  "user_id"
   end
 
   create_table "profiles", force: true do |t|
