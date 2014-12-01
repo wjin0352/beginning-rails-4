743e1c4170f4d7f712730a0c7668f852fdea74ce
won <wjin@nyit.edu>
2014-12-01 14:18:46 -0500

    added .idea to gitignore file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  resources :articles
+  root :to => "articles#index"
+  resources :articles do
+    resources :comments
+    end
+  resources :users
+  resource :session
+
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
